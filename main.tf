3103efeefdcd7a80ceca84df0bba2e9443a9b447
Shraddhaw4 <134039137+Shraddhaw4@users.noreply.github.com>
2024-05-23 19:50:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -39,77 +39,16 @@ resource "aws_key_pair" "generated_key" {
 }
 
 resource "local_file" "cloud_pem" {
-  filename = "./cloudtls.pem"
+  filename = "./aws_keys_pairs_3.pem"
   content  = tls_private_key.terraform_generated_private_key.private_key_pem
 }
 
 resource "aws_instance" "terraform_pubinstance" {
   ami           = data.aws_ami.image.id
-  key_name      = "Jenkins-Server"
+  key_name      = "aws_keys_pairs_3"
   instance_type = "t2.micro"
   iam_instance_profile = aws_iam_instance_profile.ssmp.id
   tags = {
     "Name" = "Terraform-public-server"
-  }
-  provisioner "file" {
-    source      = "./cloudtls.pem"
-    destination = "/home/ec2-user/.ssh/cloudtls.pem"
-    on_failure  = fail
-
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "cd ~/.ssh",
-      "sudo chmod 600 *.pem",
-      "echo -e 'Host *\n\tStrictHostKeyChecking no\n\tUser ec2-user\nIdentityFile /home/ec2-user/.ssh/cloudtls.pem' > config",
-      "sudo chmod 600 config"
-    ]
-    on_failure = fail
-  }
-  connection {
-    type        = "ssh"
-    user        = "ec2-user"
-    private_key = file("Jenkins-Server.pem")
-    host        = self.public_ip
-  }
-}
-
-
-resource "aws_security_group" "Terraform_private_SG" {
-  name        = "Terraform-Project-Private_SG"
-  description = "Terraform-Project-Private_SG"
-  depends_on = [
-    aws_instance.terraform_pubinstance
-  ]
-
-  ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["${aws_instance.terraform_pubinstance.private_ip}/32"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-# #private instances
-resource "aws_instance" "terraform_prvinstance" {
-  depends_on = [
-    aws_instance.terraform_pubinstance
-  ]
-  ami                    = data.aws_ami.image.id
-  key_name               = "aws_keys_pairs_3"
-  vpc_security_group_ids = [aws_security_group.Terraform_private_SG.id]
-  instance_type = "t2.micro"
-  iam_instance_profile = aws_iam_instance_profile.ssmp.id
-  tags = {
-    "Name" = "Terraform-private-server"
   }
 }
